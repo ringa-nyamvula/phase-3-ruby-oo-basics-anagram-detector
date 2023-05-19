407e51ac87bf9db03de67701b17b5efda97b1da5
ringa-nyamvula <nyamvularinga@gmail.com>
2023-05-19 09:22:05 +0300

    Complete assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+      @word = word
+    end
+
+    def match(words)
+      words.select { |w| anagram?(w) }
+    end
+
+    private
+
+    def anagram?(other_word)
+      sorted_word == sorted_chars(other_word)
+    end
+
+    def sorted_word
+      @sorted_word ||= sorted_chars(word)
+    end
+
+    def sorted_chars(str)
+      str.downcase.chars.sort
+    end
+  end
